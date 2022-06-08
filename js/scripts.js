c5fc6f63608d4057d93d2a5dfdd613f554195316
titus njuguna <titusnjuguna59@gmail.com>
2022-06-08 22:20:07 +0300

    add project 2

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,33 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+//Download resume
+/**
+ * Downloads a file
+ * @param{string} realFileId file ID
+ * @return{obj} file status
+ * */
+async function downloadFile(realFileId) {
+  // Get credentials and build service
+  // TODO (developer) - Use appropriate auth mechanism for your app
+
+  const {GoogleAuth} = require('google-auth-library');
+  const {google} = require('googleapis');
+
+  const auth = new GoogleAuth({scopes: 'https://www.googleapis.com/auth/drive'});
+  const service = google.drive({version: 'v3', auth});
+
+  fileId = realFileId;
+  try {
+    const file = await service.files.get({
+      fileId: fileId,
+      alt: 'media',
+    });
+    console.log(file.status);
+    return file.status;
+  } catch (err) {
+    // TODO(developer) - Handle error
+    throw err;
+  }
+};
